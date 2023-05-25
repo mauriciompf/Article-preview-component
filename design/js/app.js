08735d5455720ed9b4bd3060345223f975bf4268
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-05-24 21:14:29 -0400

    Update app.js

--- design/js/app.js
+++ design/js/app.js
@@ -1,29 +1,44 @@
 const shareButton = document.querySelector(".main__author-icon");
-const card = document.querySelector(".main__content");
+// rename
+const flexParent = flex.parentNode;
 const flex = document.querySelector(".main__author-flex")
+
+// how to get all children in .main__author-flex
 const divs = document.querySelectorAll(".main__author-flex > div");
 
 const shareSpan = document.createElement("span");
 const iconsSpan = document.createElement("div");
-const flexParent = flex.parentNode;
 
 const authorParent = document.createElement("div");
 
-card.appendChild(authorParent);
-authorParent.appendChild(flexParent);
-
 shareButton.addEventListener("click", () => {
-    authorParent.setAttribute("class", "main__author-parent");
-
-    flexParent.classList.toggle("author_active");
     divs.forEach(div => div.classList.toggle("divs-active"));
+    flexParent.classList.toggle("author_active");
 
+    // `
+    //     <span>
+    //         <a href="#"><strong>share</strong></a>
+    //     </span>
+    //     <div>
+    //         <a href="#" aria-label="Enter our facebook"><i class="fa-brands fa-square-facebook"></i></a>
+    //         <a href="#" aria-label="Enter our twitter"><i class="fa-brands fa-twitter"></i></a>
+    //         <a href="#" aria-label="Enter our pinterest"><i class="fa-brands fa-pinterest"></i></a>
+    //     </div>
+    // `;
 
-    shareSpan.innerHTML = `<strong>share</strong>`;
+    shareSpan.innerHTML = `<a href="#"><strong>share</strong></a>`;
     iconsSpan.innerHTML = `
-        <i class="fa-brands fa-square-facebook"></i>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-pinterest"></i>`;
+        <a href="#" aria-label="Enter our facebook"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="#" aria-label="Enter our twitter"><i class="fa-brands fa-twitter"></i></a>
+        <a href="#" aria-label="Enter our pinterest"><i class="fa-brands fa-pinterest"></i></a>`;
+
     flex.appendChild(shareSpan);
     flex.appendChild(iconsSpan);
+    iconsSpan.setAttribute("class", "main__author-icons");
+
+    if (!flexParent.classList.contains("author_active")) {
+        shareSpan.remove();
+        iconsSpan.remove();
+    }
 });
+
